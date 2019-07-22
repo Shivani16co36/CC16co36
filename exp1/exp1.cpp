06966e7c349026097469a78ea27a868f9a54543f
Shivani Naik <16co36@aitdgoa.edu.in>
2019-07-22 16:50:25 +0530

    Token prg

--- exp1/exp1.cpp
+++ exp1/exp1.cpp
@@ -1,36 +1,41 @@
 #include<iostream>
-//#include<typeinfo>
+#include<string>
+//include<typeinfo>
 using namespace std;
-
 int main()
 {
 //str[10]={a,1,2,3,4,r};
-int i;
+int i,n;
+string s, a[n];
+cout<<"Enter number of elements: ";
+cin>>n;
 
-char a[5];
-cout<<"Enter the array of characters:\n";
-for(i=0;i<5;i++)
+
+cout<<"Enter the expression:\n";
+for(i=0;i<n;i++)
 {
-cin>>a[i];
+cin>>s;
+a[i]=s;
 }
 cout<<endl;
-//int x;
-/*for(j=0;j<10;j++)
-{
 
-t=typeid(array1[j]).name();
-if(t==i)
-cout<<"integer";
-else if(t==Ss)
-cout<<"string";
-} */
-
-for(i=0;i<5;i++)
+for(i=0;i<n;i++)
 {
   if(a[i]=='0' || a[i]=='1' || a[i]=='2' || a[i]=='3' || a[i]=='4' || a[i]=='5' || a[i]=='6' || a[i]=='7' || a[i]=='8' || a[i]=='9' )
   cout<<a[i]<<" is an integer\n";
 
+  else if(a[i]=='+' || a[i]=='-' || a[i]=='=' || a[i]=='*' || a[i]=='/' || a[i]=='%' || a[i]=='<' || a[i]=='>' )
+  cout<<a[i]<<" is an operator\n";
+
+
+  else if(a[i]==';' || a[i]=='{' || a[i]=='}' || a[i]=='(' || a[i]==')' || a[i]==' ' || a[i]=='[' || a[i]==']' || a[i]=='/')
+  cout<<a[i]<<" is a delimiter\n";
+
+  else if(s=='if')
+  cout<<"Keyword\n";
   else
-  cout<<a[i]<<" is a char\n";
+  {
+  cout<<a[i]<<" is a identifier\n";
+  }
 }
 }
---
+++ expt1.cpp
@@ -0,0 +1,36 @@
+include<iostream>
+//#include<typeinfo>
+using namespace std;
+
+int main()
+{
+//str[10]={a,1,2,3,4,r};
+int i;
+
+char a[5];
+cout<<"Enter the array of characters:\n";
+for(i=0;i<5;i++)
+{
+cin>>a[i];
+}
+cout<<endl;
+//int x;
+/*for(j=0;j<10;j++)
+{
+
+t=typeid(array1[j]).name();
+if(t==i)
+cout<<"integer";
+else if(t==Ss)
+cout<<"string";
+} */
+
+for(i=0;i<5;i++)
+{
+  if(a[i]=='0' || a[i]=='1' || a[i]=='2' || a[i]=='3' || a[i]=='4' || a[i]=='5' || a[i]=='6' || a[i]=='7' || a[i]=='8' || a[i]=='9' )
+  cout<<a[i]<<" is an integer\n";
+
+  else
+  cout<<a[i]<<" is a char\n";
+}
+}
